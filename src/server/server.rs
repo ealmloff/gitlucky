0089be97fc01a1ebfb8e1bf5f935cdee2059ff1f
Ethan Almloff <ethanalmloff@gmail.com>
2025-04-05 13:58:30 -0500

    Add the capability to vote on prs

--- src/server/server.rs
+++ src/server/server.rs
@@ -7,7 +7,7 @@ use std::sync::{Arc, RwLock};
 use crate::App;
 
 #[derive(Debug, Clone)]
-struct PullRequest {
+pub struct PullRequest {
     diff_url: String,
     title: String,
     additions: usize,
@@ -16,6 +16,13 @@ struct PullRequest {
     author: String,
     repo_name: String,
     key: String,
+}
+
+#[derive(Debug, Clone)]
+struct PullRequestInfo {
+    pull_request: PullRequest,
+    left_votes: usize,
+    right_votes: usize,
 }
 
 impl PullRequest {
@@ -44,7 +51,7 @@ impl PullRequest {
 
 #[derive(Debug, Clone)]
 pub struct Server {
-    all_prs: Arc<RwLock<Vec<PullRequest>>>,
+    all_prs: Arc<RwLock<Vec<PullRequestInfo>>>,
 }
 
 impl Server {
@@ -59,10 +66,6 @@ impl Server {
         {
             router = router.serve_dioxus_application(ServeConfig::builder(), App);
         }
-        #[cfg(not(feature = "dioxus"))]
-        {
-            router = router.route("/", post(webhook_handler));
-        }
 
         let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
         axum::serve(listener, router.clone()).await.unwrap();
@@ -70,21 +73,51 @@ impl Server {
             all_prs: Arc::new(RwLock::new(Vec::new())),
         };
 
-        let s_c = server.clone();
-        router
-            .serve_dioxus_application(ServeConfig::builder(), App)
-            .route(
-                "/",
-                post(move |payload: Json<PullRequestEventPayload>| async move {
-                    s_c.webhook_handler(payload).await;
-                }),
-            );
+        #[cfg(not(feature = "dioxus"))]
+        {
+            let s_c = server.clone();
+            let _ = router
+                .serve_dioxus_application(ServeConfig::builder(), App)
+                .route(
+                    "/",
+                    post(move |payload: Json<PullRequestEventPayload>| async move {
+                        s_c.webhook_handler(payload).await;
+                    }),
+                );
+        }
         server
     }
 
     async fn webhook_handler(&self, raw_payload: Json<PullRequestEventPayload>) {
         let payload = raw_payload.0;
         let pull_request = PullRequest::new_from_payload(payload.clone());
-        self.all_prs.write().unwrap().push(pull_request.clone());
+        self.all_prs.write().unwrap().push(PullRequestInfo {
+            pull_request: pull_request.clone(),
+            left_votes: 0,
+            right_votes: 0,
+        });
     }
+
+    pub fn get_all_prs(&self) -> Vec<PullRequest> {
+        let all_prs = self.all_prs.read().unwrap();
+        all_prs.iter().map(|pr| pr.pull_request.clone()).collect()
+    }
+
+    pub fn vote_on_pr(&self, diff_url: String, direction: Direction) {
+        let mut all_prs = self.all_prs.write().unwrap();
+        for pr in all_prs.iter_mut() {
+            if pr.pull_request.diff_url == diff_url {
+                match direction {
+                    Direction::Left => pr.left_votes += 1,
+                    Direction::Right => pr.right_votes += 1,
+                }
+                break;
+            }
+        }
+    }
+}
+
+enum Direction {
+    Left,
+    Right,
 }
